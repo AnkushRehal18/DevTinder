8e0f7fbcd152a53aa544003121ab3458bec9c077
Ankush <rehalankush192@gmail.com>
2025-04-02 12:39:51 +0530

    feat:added more schema validation and learned about validate function

--- src/app.js
+++ src/app.js
@@ -12,16 +12,89 @@ app.post("/signup", async (req,res)=>{
     const user = new User(req.body);
     try{
         await user.save();
-        res.send("User added successfully");
+        res.status(200).send("User added successfully");
     }
     catch(err){
-        res.status(400).send("error saving the user " , err.message);
+        res.status(400).send(`Error saving the user: ${err.message}`);
+    }
+});
+
+// find user by email
+
+app.get("/user", async (req,res)=>{
+    const userEmail = req.body.emailId;
+    console.log(userEmail)
+    try{
+        // use to find all the occurents of the document in the collection
+        // const users = await User.find({emailId: userEmail}); 
+         // if(users.length === 0){
+        //     res.status(404).send("User not found");
+        // }
+        // else{
+        //     res.status(200).send(users);
+        // } 
+        // to find only one 
+
+        const user = await User.findOne({emailId: userEmail});
+        if(!user){
+            res.status(404).send("Cannot find the user");
+        }
+        else{
+            res.status(200).send(user); 
+        }
+         
+    }
+    catch(err){
+        res.status(400).send("Something went wrong",err);
     }
 
-    // const user = new User(userObj);
 })
 
+// getting all the data from the db 
 
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.status(200).send(users);
+    }
+    catch(err){
+        console.log("Cannot find all of the users", err);
+    }
+    
+    
+})
+
+// deleting the user form the database
+
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.status(200).send("User Deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("Cannot delete the user",err);
+    }
+})
+
+// updating the user 
+
+app.patch("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body
+
+    try{
+        const user = await User.findByIdAndUpdate({_id : userId} , data ,{
+            returnDocument :"after",
+            runValidators: true,
+            new : true
+        });
+        res.status(200).send("User updates successfully");
+    }
+    catch(err){
+        res.status(400).send("Data cannot be updated " + err.message);
+    }
+})
 
 connectDB().then(()=>{
     console.log("Database connection successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,48 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required:true,
+        minLength:3,
+        maxLength:100,
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        required:true,
+        index: true,
+        unique: true 
     },
     password:{
-        type: String
+        type: String,
+        required:true,
     },
     age: {
         type: Number,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!["Male", "Female", "others"].includes(value)) {
+                throw new Error("Gender Data is not valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://static.vecteezy.com/ti/vettori-gratis/p1/45944199-maschio-predefinito-segnaposto-avatar-profilo-grigio-immagine-isolato-su-sfondo-uomo-silhouette-immagine-per-utente-profilo-nel-sociale-media-forum-chiacchierare-in-scala-di-grigi-illustrazione-vettoriale.jpg",
+    },
+    about: {
+        type: String,
+        default: "this is the default value for the user",
+    },
+    skills: {
+        type: [String]
     }
+} ,{
+    timestamps:true
 });
 
 module.exports = mongoose.model("User",userSchema);
